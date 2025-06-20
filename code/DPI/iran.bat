f2c83cd0db3fc3f438294d2284465237a240303c
NeteR_VezeR <139502574+0netervezer0@users.noreply.github.com>
2025-06-20 16:08:47 +0300

    Update iran.bat

--- code/DPI/iran.bat
+++ code/DPI/iran.bat
@@ -8,5 +8,5 @@ set BIN=%~dp0bin\
 
 start "DPI Bypass Terminal" /min "%BIN%winws.exe" ^
 --wf-tcp=80,443 --wf-udp=443 ^
---filter-tcp=80 --hostlist="list-general.txt" --dpi-desync=fake,split2 --dpi-desync-fooling=md5sig --dpi-desync-ttl=7 --new ^
---filter-tcp=443 --hostlist-auto="list-general.txt" --hostlist-exclude="list-general.txt" --dpi-desync=split --dpi-desync-split-pos=1 --dpi-desync-fooling=badseq --dpi-desync-repeats=10 --dpi-desync-ttl=4
+--filter-tcp=80 --hostlist="lists/list-general.txt" --dpi-desync=fake,split2 --dpi-desync-fooling=md5sig --dpi-desync-ttl=7 --new ^
+--filter-tcp=443 --hostlist-auto="lists/list-general.txt" --hostlist-exclude="lists/list-general.txt" --dpi-desync=split --dpi-desync-split-pos=1 --dpi-desync-fooling=badseq --dpi-desync-repeats=10 --dpi-desync-ttl=4
